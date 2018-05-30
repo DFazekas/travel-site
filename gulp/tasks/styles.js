939c134bf1acba3bc636964a1dc9cbb4e3ffc491
Devon Fazekas <thomas-d@hotmail.ca>
2018-05-30 12:45:52 -0400

    Imported Mixins into styles.css and intergrated into _large-hero.css

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,14 +3,16 @@ var gulp = require('gulp'),
         autoprefixer = require("autoprefixer"),
         cssvars = require("postcss-simple-vars"),
         nested = require("postcss-nested"),
-        cssImport = require("postcss-import");
+        cssImport = require("postcss-import"),
+        mixins = require("postcss-mixins");
 
 gulp.task("styles", function () {
     // Pipe PostCSS style files to frameworks that translate into browser-compatible singular file.
     return gulp.src('./app/assets/styles/styles.css')
-            .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-            .on('error', function() {
+            .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+            .on('error', function(errorInfo) {
                 // Catch errors - prevent gulp from being stopped.
+                console.log(errorInfo.toString());
                 this.emit('end');
             })
             .pipe(gulp.dest('./app/temp/styles'));
