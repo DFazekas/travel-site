d73187ceb4862b8a53332a7c2f553b1d9fac960e
Devon Fazekas <thomas-d@hotmail.ca>
2018-06-03 14:50:02 -0400

    Styled site-header for desktop.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),
         cssvars = require("postcss-simple-vars"),
         nested = require("postcss-nested"),
         cssImport = require("postcss-import"),
-        mixins = require("postcss-mixins");
+        mixins = require("postcss-mixins"),
+        hexrgba = require('postcss-hexrgba');
 
 gulp.task("styles", function () {
     // Pipe PostCSS style files to frameworks that translate into browser-compatible singular file.
     return gulp.src('./app/assets/styles/styles.css')
-            .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+            .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
             .on('error', function(errorInfo) {
                 // Catch errors - prevent gulp from being stopped.
                 console.log(errorInfo.toString());
