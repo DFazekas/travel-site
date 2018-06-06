48781c83776292c5b831d228a9c1071e930b6479
Devon Fazekas <thomas-d@hotmail.ca>
2018-06-06 16:45:27 -0400

    Completed desktop and mobile header styles.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -18,6 +18,7 @@ class MobileMenu {
         // Toggle visibility modifier of the menu content.
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
         this.siteHeader.toggleClass("site-header--is-expanded");
+        this.menuIcon.toggleClass("site-header__menu-icon--close-x");
     }
 }
 
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -122,6 +122,7 @@ var MobileMenu = function () {
             // Toggle visibility modifier of the menu content.
             this.menuContent.toggleClass("site-header__menu-content--is-visible");
             this.siteHeader.toggleClass("site-header--is-expanded");
+            this.menuIcon.toggleClass("site-header__menu-icon--close-x");
         }
     }]);
 
